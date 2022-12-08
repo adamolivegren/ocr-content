67f3aa7dfb7cf1a90fbe7122490905001c6edfd4
Adam Olivegren <adamolivegren@iMac-som-tillhor-Adam.local>
2022-12-08 23:35:43 +0100

    Added functions for printing and highlighting text from the OCR scan

---
+++ js/content.js
@@ -0,0 +1,83 @@
+var ingredients = [
+  {
+    name: "Aloe vera",
+    description: "En växt som är känd för sina läkande och lugnande egenskaper",
+  },
+  {
+    name: "Shea smör",
+    description:
+      "Ett fett som utvinns från fröna på sheaträd och som är rikt på fettsyror och vitaminer",
+  },
+  {
+    name: "Jojobaolja",
+    description:
+      "En olja som utvinns från jojobafrön och som liknar hudens eget fett",
+  },
+  {
+    name: "Kokosolja",
+    description:
+      "En olja som utvinns från kokosnötter och som är rik på fettsyror och vitaminer",
+  },
+  {
+    name: "Glycerin",
+    description:
+      "En humectant (fuktbindare) som hjälper till att hålla huden återfuktad",
+  },
+  {
+    name: "Menthol",
+    description: "Jävla fint",
+  },
+  {
+    name: "Mandelolja",
+    description:
+      "En olja som utvinns från mandelnötter och som är rik på fettsyror och vitaminer",
+  },
+  {
+    name: "Lanolin",
+    description:
+      "Ett fett som utvinns från fårskinn och som är rikt på fettsyror och vitaminer",
+  },
+  {
+    name: "Eteriska oljor",
+    description:
+      "Oljor som utvinns från växter och som används för att ge produkten en doft",
+  },
+  {
+    name: "Squalane",
+    description:
+      "En olja som liknar hudens eget fett och som är rik på fettsyror och vitaminer",
+  },
+  {
+    name: "Glykolsyra",
+    description:
+      "En AHA-syra som används för att mjuka upp och exfoliera huden",
+  },
+  {
+    name: "Natriumhyaluronsyra",
+    description:
+      "En humectant (fuktbindare) som hjälper till att hålla huden återfuktad",
+  },
+  {
+    name: "C-vitamin",
+    description:
+      "Ett antioxidant som hjälper till att skydda huden mot fria radikaler",
+  },
+  {
+    name: "Vitamin E",
+    description:
+      "Ett antioxidant som hjälper till att skydda huden mot fria radikaler",
+  },
+  {
+    name: "Te-ekstrakt",
+    description:
+      "Ett extrakt som utvinns från teblad och som har antioxidantegenskaper",
+  },
+  {
+    name: "Rosenvatten",
+    description: "En källa till fukt och lukt som utvinns från rosor",
+  },
+  {
+    name: "Almondvatten",
+    description: "En källa till fukt och lukt som utvinns från mandel",
+  },
+];
